39c952f4e614bef40204bb3b050019e5b5e9e56a
TatsuyaUkai <dragonrobotu@gmail.com>
2023-10-15 11:55:59 +0900

    syukujitsu

--- dec/phase02/app/Calendar/CalendarView.php
+++ dec/phase02/app/Calendar/CalendarView.php
@@ -21,6 +21,9 @@ class CalendarView {
 	 * カレンダーを出力する
 	 */
 	function render(){
+		//HolidaySetting
+		$setting = HolidaySetting::firstOrNew();
+		$setting->loadHoliday($this->carbon->format("Y"));
 		$html = [];
 		$html[] = '<div class="calendar">';
 		$html[] = '<table class="table">';
@@ -41,7 +44,7 @@ class CalendarView {
 		$weeks = $this->getWeeks();
 		foreach($weeks as $week){
 			$html[] = '<tr class="'.$week->getClassName().'">';
-			$days = $week->getDays();
+			$days = $week->getDays($setting);
 			foreach($days as $day){
 				$html[] = '<td class="'.$day->getClassName().'">';
 				$html[] = $day->render();
--- dec/phase02/app/Calendar/CalendarWeekDay.php
+++ dec/phase02/app/Calendar/CalendarWeekDay.php
@@ -4,9 +4,21 @@ use Carbon\Carbon;
 
 class CalendarWeekDay {
 	protected $carbon;
+	protected $isHoliday = false;
 
 	function __construct($date){
 		$this->carbon = new Carbon($date);
+	}
+
+	function getClassName(){
+		$classNames = [ "day-" . strtolower($this->carbon->format("D")) ];
+
+		//祝日フラグを出す
+		if($this->isHoliday){
+			$classNames[] = "day-close";
+		}
+
+		return implode(" ", $classNames);
 	}
 
 	function getClassName(){
